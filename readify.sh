27533407986668aee04ff502a02b481ad77c752c
Bogdan-Mihai Sandu <bogdan.sandu92@gmail.com>
2015-10-05 08:01:52 +0300

    Terminator, curl, vim, vimrc

---
+++ readify.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+which terminator || sudo apt-get install terminator
+which curl || sudo apt-get install curl
+which vim || sudo apt-get install vim
+
+if [ ! -d ~/.vim/autoload -a ! -d ~/.vim/bundle ]
+then
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+if [ ! -d ~/.vim/bundle/tabular ]
+then
+    cd ~/.vim/bundle
+    git clone git://github.com/godlygeek/tabular.git
+    echo "Installed tabular!"
+else
+    echo "Tabular already installed..."
+fi
+
+if [ ! -d ~/.vim/bundle/vim-surround ]
+then
+    cd ~/.vim/bundle
+    git clone git://github.com/tpope/vim-surround.git
+    echo "Installed vim-surround!"
+else
+    echo "Vim-surround already installed..."
+fi
+
+if [ ! -d ~/.vim/bundle/ctrlp.vim ]
+then
+    cd ~/.vim/bundle
+    git clone https://github.com/kien/ctrlp.vim.git
+    echo "Installed CtrlP!"
+else
+    echo "CtrlP already installed..."
+fi
+
+echo "\"Some cool tips'n'tricks:
+\":%!xxd and :%!xxd -r transform and revert vim into a hex editor
+\":.! inserts the output of an external command into the current document
+\"viw - selects the current word
+\"diw - viw + delete;
+\"ciw - diw + insert;
+\"de - delete till the end of the word
+\"ZZ - save'n'close
+\"zz - scrolls enough to view the current line in the middle
+\"D - delete the rest of the line
+\"C - cuts the rest of the line and switch to insert
+\"dt[char] delete to the next char
+\"bye - copies the current word
+\"vi[bracket = <[{(][p] selects everything inside the bracked. p also replaces it.
+\"ci and di are variations of the command above
+\"ctrl+v in insert mode inserts the actual valuea of the key (overrides vimrc)
+\"U - uppercase selected
+\"u - lowercase selected
+\"gv - selects last visual selection
+\"Ctrl+v - block selection mode
+\"V - line selection mode (selects current line)
+\"dd - V + delete
+\"cc - dd + insert
+\"q: - command history
+\"gg - beginning of file
+\"G - end of file
+\":for i in range(1,255) | .put='10.0.0.'.i | endfor - inserts a range of ips
+\"search: /[pattern]
+\"replace stuff: vimregex.com
+\"another trick: while in block selection, you can select a vertical block, then
+\"enter I, type something and then press <esc>. This inserts your text on all the
+\"lines contained by the block selection.
+\"
+\"About folding: 
+\"Shift+V -> select lines -> zf makes a fold
+\"otherwise, zf[#lines]j
+\"za -> toggles the fold
+\"zj, zk -> moves to next/previous fold;
+\"zd -> deletes the fold
+\"zE -> deletes all folds
+\"zR, zM -> open/closes all folds;
+\"
+\"About CtrlP:
+\"Ctrl + P -> opens CtrlP (what a surprise)
+\"<c-t> - open in new tab
+\"<c-v>, <c-x> - open in split
+\"<c-b> - switch modes
+\"<c-d> - search only filename
+\"<c-r> - regex
+\":cd [path] in normal mode to change ctrlp path
+
+set tabstop=8
+set softtabstop=4
+set shiftwidth=4
+set shiftround
+set expandtab
+
+set autoindent
+set smartindent
+
+:syntax on
+
+set number
+set hlsearch
+set incsearch
+set foldenable
+set runtimepath^=~/.vim/bundle/ctrlp.vim
+
+let g:ctrlp_working_path_mode = 0
+
+execute pathogen#infect()" > ~/.vimrc
