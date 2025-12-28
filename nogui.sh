e216b5d9614c5d9e2dc68960315d74ba3bd3158f
Bogdan Sandu <bogdan.sandu92@gmail.com>
2025-12-28 18:10:40 +0100

    Remove GUI things

--- nogui.sh
+++ nogui.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
 function install_nogui {
-    sudo apt-get update
-
+    sudo apt update
     sudo ./install_vim.sh
-    sudo ./install_tmux.sh
 
     source ./utils.sh
 
-    install_deb "curl"
     install_deb "htop"
-    install_deb "openssh-server"
-    install_deb "git"
+    install_deb "tmux"
 }
 
 install_nogui
